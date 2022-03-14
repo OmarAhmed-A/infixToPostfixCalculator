b26384cc2e5058342face0de586970fb21f3803b
Omar Ahmed <omarahmed170@gmail.com>
2022-03-14 16:30:24 +0200

    first commit

---
+++ charStack.h
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Stack
+{
+	char* arr;
+	int sp;
+	int size;
+
+	//Constructor (from C++)
+	Stack()
+	{
+		arr = NULL;
+		sp = -1;
+		size = 0;
+	}
+};
+
+bool initializeStack(Stack *s, int size)
+{
+	if (s->arr != NULL) //In case previous malloc is called
+		free(s->arr);
+	s->arr = (char*)malloc(sizeof(char) * size);
+	if (s->arr == NULL)
+		return false;
+	s->size = size;
+	s->sp = -1;
+	return true;
+}
+
+int push(Stack *s, char item)
+{
+	if (s->arr == NULL)
+		return -1;	//Stack not initialized !
+	if (s->sp + 1 == s->size)
+		return -2;	//Stack overflow !
+
+	s->sp = s->sp + 1;	//or s->sp++;
+	s->arr[s->sp] = item;
+	return 0;
+}
+
+int pop(Stack *s, char* item)
+{
+	if (s->arr == NULL)
+		return -1;	//Stack not initialized !
+	if (isEmpty(s))
+		return -2;	//Stack underflow !
+
+	*item = s->arr[s->sp];
+	s->sp = s->sp - 1;	//s->sp--;
+	return 0;
+}
+
+int isEmpty(Stack* s)
+{
+	return s->sp == -1;
+}
+
+int peek(Stack* s, char *item)
+{
+    if (!isEmpty(s))
+    *item = s->arr[s->sp];
+
+    return 1;
+}
