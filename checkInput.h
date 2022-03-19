d8ec7575f1b4a9e413cc19fb3c5931095e3192c4
Omar Ahmed <omarahmed170@gmail.com>
2022-03-19 18:00:40 +0200

    finished check input messages

--- checkInput.h
+++ checkInput.h
@@ -1,5 +1,6 @@
 #include "charStack.h"
 #include <string.h>
+#include <ctype.h>
 
 int checkBrackets(char *input)
 {
@@ -25,10 +26,21 @@ int checkBrackets(char *input)
     return 0; // bancled
 }
 
-
 int checkInput(char *input)
 {
-    
 
+    if (input[strlen(input)-1] == '+' || input[strlen(input)-1] == '-' ||
+        input[strlen(input)-1] == '*' || input[strlen(input)-1] == '/')
+        return -1; // last char of input is operator // not allowed
 
+    for (int i = 0; i < strlen(input); i++)
+    {
+        if (isdigit(input[i]) && isdigit(input[i + 1]))
+            return -2; // multidigit input num //not allowed
+        else if ((input[i] == '+' || input[i] == '/' || input[i] == '*' || input[i] == '-') 
+            &&  (input[i+1] == '+' || input[i+1] == '/' || input[i+1] == '*' || input[i+1] == '-'))
+            return -3; // multiple operators in a row //not allowed
+    }
+
+    return 0; // all ok
 }
